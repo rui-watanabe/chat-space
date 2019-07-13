8669210ad2437d108bf648cc829feb9a090f1d18
rui-watanabe <wataru610806@gmail.com>
2019-07-13 19:06:18 +0900

    messages update

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,99 @@
+$(function(){
+  function buildMessage(message){
+    var img = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+    var html = `<div class="message">
+                <div class="upper-message">
+                <div class="upper-message__user-name">
+                  ${message.user_name}
+                </div>
+                <div class="upper-message__date">
+                  ${message.created_at}
+                </div>
+                </div>
+                <div class="lower-message">
+                <p class="lower-message__content">
+                  ${message.content}
+                </p>
+                  ${img}
+                </div>
+                </div>`
+    return html;
+  }
+
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+  .done(function(data){
+      var html = buildMessage(data);
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('#message_content').val("");
+      $('#message_image').val("");
+    })
+  .fail(function(){
+    alert('error');
+    })
+  })
+});
+
+
+
+
+// $(function(){
+//   function buildMessage(message){
+    // var img = "";
+    // if (message.image){
+    // var img =  `<img class="lower-message__image" src"${message.image}>`
+    // }
+    // var html = `<div class="message">
+    //             <div class="upper-message">
+    //             <div class="upper-message__user-name">
+    //               ${message.user_name}
+    //             </div>
+    //             <div class="upper-message__date">
+    //               ${message.created_at}
+    //             </div>
+    //             </div>
+    //             <div class="lower-message">
+    //             <p class="lower-message__content">
+    //               ${message.content}
+    //               ${img}
+    //             </p>
+    //             </div>
+    //             </div>`
+    // return html;
+//   }
+  
+//   $("#new_message").on("submit", function(e){
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action')
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: "json",
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildMessage(data);
+//       $('.message').append(html)
+//       $('.submit').prop("disabled", false)
+//       $('#message_content').val("");
+//       $('#message_image').val("");
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+//   })
+// });
